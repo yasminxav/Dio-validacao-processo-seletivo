92de2764392dbe9b92b904106bddfb25ee7037a9
Yasmin Gabriele Xavier <ygxavier.yx@gmail.com>
2024-10-26 17:00:32 -0300

    Correções

---
+++ ValidacaoProcessoSeletivo/DesafioControleFluxo/src/Contador.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Contador {
+	public static void main(String[] args) {
+		Scanner terminal = new Scanner(System.in);
+		System.out.println("Digite o primeiro parâmetro");
+		int parametroUm = terminal.nextInt();
+		System.out.println("Digite o segundo parâmetro");
+		int parametroDois = terminal.nextInt();
+        terminal.close();
+		
+		try {
+			//chamando o método contendo a lógica de contagem
+			contar(parametroUm, parametroDois);
+		}catch (ParametrosInvalidosException exception) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+		}		
+	}
+	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+		if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException(); 
+        }
+		int contagem = parametroDois - parametroUm;
+        for (int indice = 1; indice <= contagem; indice++) {
+            System.out.println("Imprimindo o número " + indice);
+        }
+	}
+}
